b0c96f994bde53fbef4bc589568b460dca1b1cd8
Isaiah Harris <harrisfambam@gmail.com>
2019-01-17 15:18:39 -1000

    add index, styles, and app files

---
+++ app.js
@@ -0,0 +1,6 @@
+// $('.menu-icon').click(function() {
+//   $('.panel').toggleClass('show-menu');
+//   $('.menu').toggleClass('active');
+//   $('.circle-1').toggleClass('slide');
+//   $('.dash-top').toggleClass('slide-top');
+// });
